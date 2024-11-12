d4716d2bd59f27a8d49d432e4a34c0b989b9089d
Gabriel de Oliveira Ramos <35896514+fokerg@users.noreply.github.com>
2024-11-12 02:48:39 +0000

    for olga2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-   return <h1>Para Olga</h1>
-   <p>Que as dificuldades da vida nunca seja maior que a força que nos move: o AMOR</p>
-   <h2>Eu SEMPRE estarei aqui para você.</h2> 
+   return <><h1>Para Olga</h1><p>Que as dificuldades da vida nunca seja maior que a força que nos move: o AMOR</p><h2>Eu SEMPRE estarei aqui para você.</h2></> 
 }
 
 export default Home;
