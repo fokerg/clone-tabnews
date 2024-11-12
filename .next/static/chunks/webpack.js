4ddca244e75260ddcc68602c07488ce93e766868
Gabriel de Oliveira Ramos <35896514+fokerg@users.noreply.github.com>
2024-11-12 02:26:10 +0000

    mensagem para olga.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e38018cf5725f620"; }
+/******/ 		__webpack_require__.h = function() { return "3d95f7409cce1e4c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
